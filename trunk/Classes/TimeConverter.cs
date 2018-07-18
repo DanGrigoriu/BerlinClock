6ba95a7a68135600a8790fbb0d1e86924774b1c3
DanGrigoriu <41388807+DanGrigoriu@users.noreply.github.com>
2018-07-18 21:06:25 +0300

    Add files via upload
    First commit. Minimum viable product commit.
    TODO
    - validations (including error messages)
    - refactor repetitive code
    - externalize resources (strings, hardcoded characters, newLine etc.)
    - extra tests

---
+++ trunk/Classes/TimeConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BerlinClock
+{
+    public class TimeConverter : ITimeConverter
+    {
+        public string convertTime(string aTime)
+        {
+            string[] timeParts = aTime.Split(':');
+            string hoursStr = timeParts[0];
+            string minutesStr = timeParts[1];
+            string secondsStr = timeParts[2];
+            int hours = Convert.ToInt32(hoursStr);
+            int minutes = Convert.ToInt32(minutesStr);
+            int seconds = Convert.ToInt32(secondsStr);
+
+            StringBuilder ret = new StringBuilder();
+            if (seconds % 2 == 0) ret.Append("Y\r\n");
+            else ret.Append("O\r\n");
+
+            for (int i = 0; i < 4; i++)
+                if (hours / 5 > i)
+                    ret.Append("R");
+                else ret.Append("O");
+            ret.Append("\r\n");
+            for (int i = 0; i < 4; i++)
+                if (hours % 5 > i)
+                    ret.Append("R");
+                else ret.Append("O");
+            ret.Append("\r\n");
+
+
+            for (int i = 0; i < 11; i++)
+                if (minutes / 5 > i)
+                    if ((i + 1) % 3 == 0)
+                        ret.Append("R");
+                    else ret.Append("Y");
+                else ret.Append("O");
+
+            ret.Append("\r\n");
+            for (int i = 0; i < 4; i++)
+                if (minutes % 5 > i)
+                    ret.Append("Y");
+                else ret.Append("O");
+
+            return ret.ToString();
+        }
+    }
+}
