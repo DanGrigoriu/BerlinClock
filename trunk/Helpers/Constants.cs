d80d5f25bb7ef74cf551fcfbbf5a2ddcd3a3aafd
DanGrigoriu <41388807+DanGrigoriu@users.noreply.github.com>
2018-08-13 12:05:51 +0300

    included missing files

---
+++ trunk/Helpers/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace BerlinClock.Helpers
+{
+    public class Constants
+    {
+        public static char Colon = ':';
+        public static int HoursDivider = 5;
+        public static int MinutesDivider = 5;
+        public static int MaxHours = 24;
+        public static int MaxMinutes = 59;
+        public static int IndexMinuteMultipleDifferentColor = 3;
+    }
+}
